4c04fe20c4a7a083f2eead7fdda8e295844b3b80
gusti-hub <agustianggaraputra@gmail.com>
2025-02-06 23:13:58 +0700

    commit

--- routes/profileRoutes.js
+++ routes/profileRoutes.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const { changePassword } = require('../controllers/ProfileController');
+const { changePassword } = require('../controllers/profileController');
 
 // Route for changing password
 router.post('/change-password', changePassword);
